e3932e85ff231b0f62fee09a861f6cad50c8c751
Paulo Black <paulo.black@homes.com>
2017-02-20 22:56:26 -0500

    bg color

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
 
 import { Column } from 'hedron';
+import { Tooltip } from 'reactstrap';
 
 import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
@@ -13,12 +14,29 @@ const HeaderColumn = styled(Column)`
 `;
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            tooltipOpen: false,
+        };
+    }
+
+    toggle() {
+        this.setState({
+            tooltipOpen: !this.state.tooltipOpen,
+        });
+    }
     render() {
         return (
           <HeaderColumn>
             <NavBar>
-              <HeaderLink href="https://www.pauloblack.com">
+              <HeaderLink id="tooltip" href="https://www.pauloblack.com">
                 <FormattedMessage {...messages.home} />
+                <Tooltip placement="left" isOpen={this.state.tooltipOpen} target="tooltip" toggle={this.toggle}>
+                Blep
+                </Tooltip>
               </HeaderLink>
               <HeaderLink href="https://boils.pauloblack.com">
                 <FormattedMessage {...messages.boilerplate} />
