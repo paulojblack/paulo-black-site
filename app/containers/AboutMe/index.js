2342c9cdf87f36bab576a93933f15fa9f4a3ca2f
Paulo Black <paulo.black@homes.com>
2017-02-21 21:01:32 -0500

    back button for about me

--- app/containers/AboutMe/index.js
+++ app/containers/AboutMe/index.js
@@ -2,13 +2,20 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
 import { Page, Row, Column } from 'hedron';
 
 import H1 from 'components/H1';
 import PauloRockPic from 'assets/paulorockpic.jpg';
 import DeepThought from 'assets/deepthought.jpg';
+
+import BackButton from './BackButton';
 import Img from './Img';
 import messages from './messages';
+
+const SpecialColumn = styled(Column)`
+  padding: 0px;
+`;
 
 export class AboutMe extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
     render() {
@@ -21,11 +28,16 @@ export class AboutMe extends React.PureComponent { // eslint-disable-line react/
               ]}
             />
             <Row>
-              <Column>
+              <SpecialColumn>
                 <H1>
                   <FormattedMessage {...messages.header} />
                 </H1>
-              </Column>
+              </SpecialColumn>
+              <SpecialColumn>
+                <BackButton id="No" to="/">
+                  {'No I\'m not, get me out of here'}
+                </BackButton>
+              </SpecialColumn>
               <Column>
                 <p>
                   <FormattedMessage {...messages.firstbody} />
